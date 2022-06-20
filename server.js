ed0ce9365b8ecb1cb45b6b702e4884e46e98dd95
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2022-06-20 15:09:28 -0400

    connect swagger to express #1

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const routes = require('./controllers');
+const express = require("express");
+const routes = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerDoc = require("./swagger.json");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+app.use("/docs", swaggerUi.serveFiles(swaggerDoc), swaggerUi.setup(swaggerDoc));
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
