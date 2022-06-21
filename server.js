1bc7f5c965cbe05da58a5310b837e4b513954183
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2022-06-21 00:48:03 -0400

    added swagger options to hide schemas #1

--- server.js
+++ server.js
@@ -6,13 +6,20 @@ const PORT = process.env.PORT || 3001;
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerDoc = require("./swagger.json");
+const swaggerOptions = {
+  defaultModelsExpandDepth: -1,
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-app.use("/docs", swaggerUi.serveFiles(swaggerDoc), swaggerUi.setup(swaggerDoc));
+app.use(
+  "/docs",
+  swaggerUi.serveFiles(swaggerDoc, { swaggerOptions }),
+  swaggerUi.setup(swaggerDoc)
+);
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
