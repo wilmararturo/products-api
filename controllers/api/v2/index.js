933b35551e31c62d9cb9e9edd876f557f6ff7e69
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2022-07-17 14:56:56 -0400

    implemented cache control for GET

--- controllers/api/v2/index.js
+++ controllers/api/v2/index.js
@@ -3,6 +3,13 @@ const categoryRoutes = require("./categories");
 const productRoutes = require("./products");
 const reviewRoutes = require("./reviews");
 
+router.use((req, res, next) => {
+  if (req.method === "GET") {
+    res.set("Cache-Control", "private, max-age=300");
+  }
+  next();
+});
+
 router.use("/categories", categoryRoutes);
 router.use("/products", productRoutes);
 router.use("/reviews", reviewRoutes);
